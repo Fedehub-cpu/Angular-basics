c7e1446eccae389da885ea164ca62017f5fbbd7c
fcuadrat <fcuadrat@emeal.nttdata.com>
2022-05-26 11:46:16 +0200

    Curso de Angular

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string []=['Spiderman', 'Ironman', 'Hulk','Thor'];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+
+    const heroeBorrado = this.heroes.shift();
+
+    console.log(heroeBorrado);
+
+  }
+
+  mostrarBorrado() {
+    this.heroeBorrado = this.heroes.shift() || "";
+
+  }
+
+}
